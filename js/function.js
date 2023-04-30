335ed2e34f8da4ac79ea8d2595635145d4873c13
Alice Izabeli <ereyzeel.nathr@gmail.com>
2023-04-30 07:23:23 -0300

    add map api js

--- js/function.js
+++ js/function.js
@@ -6,13 +6,32 @@ window.onload = function(){
             center: new google.maps.LatLng(-27.648598 , -48.577423 ),
             scrollWheel:false,
             zoom:12,
-            MapTypeId: google.maps.MapTypeId.ROADMAP
+            mapTypeId: google.maps.MapTypeId.ROADMAP
 
         }
 
         map = new google.maps.Map(document.getElementById("mapa"),mapProp)
     }
 
+    function addMarker(lat,long,icon,content){
+        var latLng = {'lat':lat,'lng':long}
+        var marker = new google.maps.Marker({
+            position:latLng,
+            map:map,
+            icon:icon
+        })
+
+        var infoWindow = new google.maps.InfoWindow({
+            content:content,
+            maxWidth:200,
+            pixelOffSet: new google.maps.Size(0,20)
+        })
+
+        infoWindow.open(map,marker)
+    }
+
     initialize();
 
+    var conteudo = '<p style="color:black;font-size:13px;padding:10px 0;">Meu Endereço</p>';
+    addMarker(-27.5956863,-48.5526779,'',conteudo);
 }
